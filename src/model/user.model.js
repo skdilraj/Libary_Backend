b0d9b3e286fb8de1d384d954626b099afcee6b83
skdilraj <anud7067@gmail.com>
2025-07-19 20:18:16 +0530

    model update

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,10 +1,12 @@
 // models/User.js
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt"
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true ,trim: true,index:true},
-  email: { type: String, required: true, unique: true ,trim: true,index:true},
+  email: { type: String, required: true, unique: true ,trim: true},
   password: { type: String, required: true },
   role: {
     type: String,
@@ -24,7 +26,48 @@ const userSchema = new Schema({
     type: String,
     required: true,
     enum: ['AdminProfile', 'LibrarianProfile', 'StudentProfile'] // collections name
+  },
+   refreshToken: {
+    type: String
   }
 }, { timestamps: true });
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
 
 module.exports = mongoose.model('User', userSchema);
